d72ba3041bbc4a0cced0ba3df900f941e8ffeb3e
Kakmanzoo1103 <103602848+Kakmanzoo1103@users.noreply.github.com>
2024-02-12 02:45:42 -0500

    ._.

---
+++ finkrent/src/main.jsx
@@ -0,0 +1,144 @@
+import React from "react";
+import "./style.css";
+
+export const Index = () => {
+  return (
+    <div className="index">
+      <div className="div">
+        <div className="overlap">
+          <img
+            className="sun"
+            alt="Sun"
+            src="https://cdn.animaapp.com/projects/65c983f690bc759cfdc5ddcb/releases/65c9848581257e46c59d2778/img/sun@2x.png"
+          />
+          <img
+            className="bandera-colombia"
+            alt="Bandera colombia"
+            src="https://cdn.animaapp.com/projects/65c983f690bc759cfdc5ddcb/releases/65c9848581257e46c59d2778/img/bandera-colombia.png"
+          />
+          <div className="text-wrapper">COP</div>
+          <div className="overlap-group">
+            <img
+              className="user"
+              alt="User"
+              src="https://cdn.animaapp.com/projects/65c983f690bc759cfdc5ddcb/releases/65c9848581257e46c59d2778/img/user@2x.png"
+            />
+            <div className="text-wrapper-2">Inicia Sesión</div>
+          </div>
+          <div className="overlap-2">
+            <div className="text-wrapper-3">Registrate</div>
+            <img
+              className="checked-user-male"
+              alt="Checked user male"
+              src="https://cdn.animaapp.com/projects/65c983f690bc759cfdc5ddcb/releases/65c9848581257e46c59d2778/img/checked-user-male@2x.png"
+            />
+          </div>
+          <div className="div-wrapper">
+            <div className="text-wrapper-4">Inicio</div>
+          </div>
+          <div className="overlap-3">
+            <div className="text-wrapper-5">Nosotros</div>
+          </div>
+          <div className="overlap-4">
+            <div className="text-wrapper-6">Reservar</div>
+          </div>
+          <div className="overlap-5">
+            <div className="text-wrapper-7">Contacto</div>
+          </div>
+        </div>
+        <div className="overlap-group-2">
+          <img
+            className="barra-inferior"
+            alt="Barra inferior"
+            src="https://cdn.animaapp.com/projects/65c983f690bc759cfdc5ddcb/releases/65c9848581257e46c59d2778/img/barra-inferior.svg"
+          />
+          <div className="text-wrapper-8">Todos los derechos reservados</div>
+          <img
+            className="copyright"
+            alt="Copyright"
+            src="https://cdn.animaapp.com/projects/65c983f690bc759cfdc5ddcb/releases/65c9848581257e46c59d2778/img/copyright@2x.png"
+          />
+          <div className="text-wrapper-9">2024 FINKRENT</div>
+          <div className="text-wrapper-10">Copyright</div>
+          <div className="text-wrapper-11">Contactanos</div>
+          <div className="text-wrapper-12">E-mail: FINKRENTCOL@gmail.com</div>
+          <p className="p">Teléfono Móvil: (601) 319 6394346</p>
+          <div className="flexcontainer">
+            <p className="text">
+              <span className="span">
+                Dirección: Cra. 7 #35-85
+                <br />
+              </span>
+            </p>
+            <p className="text">
+              <span className="span">Bogotá - Colombia</span>
+            </p>
+          </div>
+          <img
+            className="image"
+            alt="Image"
+            src="https://cdn.animaapp.com/projects/65c983f690bc759cfdc5ddcb/releases/65c9848581257e46c59d2778/img/image-2.png"
+          />
+        </div>
+        <div className="overlap-6">
+          <div className="rectangle" />
+          <div className="botn-reserva" />
+          <div className="text-wrapper-13">Reserva tu vacaciones</div>
+          <p className="text-wrapper-14">Nosotros nos encargamos de tus vacaciones o escapadas</p>
+          <div className="text-wrapper-15">VIVE UNA NUEVA AVENTURA</div>
+          <div className="text-wrapper-16">Reservar</div>
+        </div>
+        <div className="overlap-7">
+          <p className="text-wrapper-17">Para ingresar a la API de la pagina, haz click en el botón</p>
+          <div className="overlap-8">
+            <div className="text-wrapper-18">Entrar a la API</div>
+          </div>
+        </div>
+        <div className="overlap-9">
+          <div className="text-wrapper-19">Eje Cafetero</div>
+        </div>
+        <div className="overlap-10">
+          <div className="text-wrapper-20">Antioquia</div>
+        </div>
+        <div className="overlap-11">
+          <div className="text-wrapper-21">Llanos Orientales</div>
+        </div>
+        <div className="otros-lugares-wrapper">
+          <div className="otros-lugares">
+            Otros Lugares
+            <br />
+            Interesantes
+          </div>
+        </div>
+        <div className="overlap-12">
+          <p className="text-wrapper-22">No dejes pasar esta oportunidad</p>
+          <div className="flexcontainer-2">
+            <p className="span-wrapper">
+              <span className="text-wrapper-23">
+                Regístrate ahora y obtén <br />
+              </span>
+            </p>
+            <p className="text-2">
+              <span className="text-wrapper-23">un </span>
+              <span className="text-wrapper-24">20%</span>
+              <span className="text-wrapper-23"> en tu primera reserva</span>
+            </p>
+          </div>
+          <div className="overlap-13">
+            <div className="text-wrapper-25">Registrate Ahora</div>
+          </div>
+        </div>
+        <img
+          className="viajar"
+          alt="Viajar"
+          src="https://cdn.animaapp.com/projects/65c983f690bc759cfdc5ddcb/releases/65c9848581257e46c59d2778/img/viajar-1.png"
+        />
+        <img
+          className="img"
+          alt="Sun"
+          src="https://cdn.animaapp.com/projects/65c983f690bc759cfdc5ddcb/releases/65c9848581257e46c59d2778/img/sun@2x.png"
+        />
+      </div>
+    </div>
+  );
+};
